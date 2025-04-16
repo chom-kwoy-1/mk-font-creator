43b865fa4cd9df86ab35f56c4499c4229de00e4e
Chom Kwoy <chom.kwoy@xn--gt1b.xyz>
2025-04-16 11:33:10 -0400

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,119 @@
-import Image from "next/image";
+'use client'
+import React from "react";
+import { styled } from '@mui/material/styles';
+import {Box, Button, LinearProgress} from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [file, setFile] = React.useState<File | null>(null);
+    const [loadingState, setLoadingState] = React.useState<string | null>(null);
+    const [loadDone, setLoadDone] = React.useState<boolean>(false);
+    const [doc, setDoc] = React.useState<Document | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    async function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (event.target.files && event.target.files.length > 0) {
+            // Reset state
+            setLoadDone(false);
+            setLoadingState(null);
+            setDoc(null);
+
+            const newFile = event.target.files[0];
+            setFile(newFile);
+
+            const formData = new FormData();
+            formData.append('file', newFile as Blob);
+
+            setLoadingState("Uploading file");
+            const response = await fetch('/api/convert_font', {
+                method: 'POST',
+                body: formData,
+            });
+            setLoadingState("Processing response");
+
+            if (response.body instanceof ReadableStream) {
+                const stream = response.body;  // get gzip stream
+
+                const reader = stream.pipeThrough(new DecompressionStream('gzip')).getReader();
+
+                let length = 0;
+                let text = "";
+                const decoder = new TextDecoder();
+                while (true) {
+                    const {value, done} = await reader.read();  // start reading
+                    if (done) {
+                        setLoadingState("Downloaded: " + length + " bytes");
+                        break;
+                    }
+
+                    length += value.length;
+                    setLoadingState("Downloading: " + length + " bytes");
+
+                    text += decoder.decode(value);
+                }
+
+                setLoadingState("Parsing XML...");
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(text, 'text/xml');
+                setLoadingState("Loading Done.");
+
+                setDoc(doc);
+                setLoadDone(true);
+            }
+            else {
+                await response.json().then((data) => {
+                    console.error(data);  // TODO: Show error to user
+                }).catch((err) => {
+                    console.error(err);  // TODO: Show error to user
+                });
+            }
+        }
+    }
+
+    return (
+        <React.Fragment>
+            {file && <p>File: {file.name}</p>}
+            <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+                loading={loadingState !== null && !loadDone}
+            >
+                Upload font file
+                <VisuallyHiddenInput
+                    type="file"
+                    onChange={(event) => handleFileChange(event)}
+                />
+            </Button>
+            {loadingState && <Box>
+                {!loadDone && <LinearProgress />}
+                Loading: {loadingState}
+            </Box>}
+            {loadDone && doc && <ShowFontContents doc={doc} />}
+        </React.Fragment>
+    );
+}
+
+
+function ShowFontContents({doc}: {doc: Document}) {
+    return (
+        <React.Fragment>
+            <p><strong>Font</strong>: {doc.querySelector('namerecord[nameID="4"]')?.innerHTML}</p>
+            <p><strong>Version</strong>: {doc.querySelector('namerecord[nameID="5"]')?.innerHTML}</p>
+            <p><strong>Number of glyphs</strong>: {doc.querySelectorAll('GlyphID').length}</p>
+        </React.Fragment>
+    );
 }
