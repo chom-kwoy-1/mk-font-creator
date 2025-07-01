c40e3bfda74de3127988236911e51e017e27d7e4
Chom Kwoy <chom.kwoy@xn--gt1b.xyz>
2025-07-02 01:36:44 +0900

    fix resize

--- src/app/ResizeableRect.tsx
+++ src/app/ResizeableRect.tsx
@@ -18,7 +18,8 @@ export function ResizeableRect(
         rescale: (p: Point) => number[],
         xyScales: { x: number, y: number },
         children?: React.ReactNode,
-    } & Konva.LineConfig>) {
+    } & Konva.LineConfig>
+) {
     const [isDragging, setIsDragging] = React.useState({
         bottom: false,
         right: false,
@@ -52,38 +53,26 @@ export function ResizeableRect(
     }
 
     const dragColor = props.stroke;
-
-    const horzLineProps: Konva.LineConfig = {
-        onMouseEnter: (e: Konva.KonvaEventObject<MouseEvent>) => {
-            const container = e.target.getStage()?.container();
-            if (container) {
-                container.style.cursor = "ns-resize";
-            }
-        },
-        onMouseLeave: (e: Konva.KonvaEventObject<MouseEvent>) => {
-            const container = e.target.getStage()?.container();
-            if (container) {
-                container.style.cursor = "default";
-            }
-        },
-    };
-    const vertLineProps: Konva.LineConfig = {
-        onMouseEnter: (e: Konva.KonvaEventObject<MouseEvent>) => {
-            const container = e.target.getStage()?.container();
-            if (container) {
-                container.style.cursor = "ew-resize";
-            }
-        },
-        onMouseLeave: (e: Konva.KonvaEventObject<MouseEvent>) => {
-            const container = e.target.getStage()?.container();
-            if (container) {
-                container.style.cursor = "default";
-            }
-        },
-    };
-
     const handleSize = 7;
     const handleColor = amber[500];
+
+    function updateRect(coords: {x1?: number, y1?: number, x2?: number, y2?: number}) {
+        const newX1 = coords.x1 ?? x1;
+        const newY1 = coords.y1 ?? y1;
+        const newX2 = coords.x2 ?? x2;
+        const newY2 = coords.y2 ?? y2;
+        topRef.current?.points([newX2, newY2, newX1, newY2]);
+        bottomRef.current?.points([newX1, newY1, newX2, newY1]);
+        rightRef.current?.points([newX2, newY1, newX2, newY2]);
+        leftRef.current?.points([newX1, newY2, newX1, newY1]);
+
+        const scaleX = (newX2 - newX1) / (x2 - x1);
+        const scaleY = (newY2 - newY1) / (y2 - y1);
+        childRef.current?.x(-x1 * scaleX + newX1);
+        childRef.current?.y(-y1 * scaleY + newY1);
+        childRef.current?.scaleX(scaleX);
+        childRef.current?.scaleY(scaleY);
+    }
 
     return (
         <Group>
@@ -110,10 +99,8 @@ export function ResizeableRect(
                     // rightRef.current?.points([x2 + rx, y1 + ry, x2 + rx, y2 + ry]);
                     // topRef.current?.points([x2 + rx, y2 + ry, x1 + rx, y2 + ry]);
                     // leftRef.current?.points([x1 + rx, y2 + ry, x1 + rx, y1 + ry]);
-                    if (childRef.current !== null) {
-                        childRef.current.x(rx);
-                        childRef.current.y(ry);
-                    }
+                    childRef.current?.x(rx);
+                    childRef.current?.y(ry);
                 }}
                 onDragEnd={(e) => {
                     setIsDragging({...isDragging, whole: false});
@@ -128,10 +115,10 @@ export function ResizeableRect(
                     updateNext.current = () => {
                         groupRef.current?.offset({x: 0, y: 0});
                         e.target.setPosition({x: 0, y: 0});
-                        if (childRef.current !== null) {
-                            childRef.current.x(0);
-                            childRef.current.y(0);
-                        }
+                        childRef.current?.x(0);
+                        childRef.current?.y(0);
+                        childRef.current?.scaleX(1);
+                        childRef.current?.scaleY(1);
                     };
                 }}
                 onMouseEnter={(e) => {
@@ -158,35 +145,32 @@ export function ResizeableRect(
                     stroke={isDragging.bottom ? dragColor : props.stroke}
                     strokeWidth={isDragging.bottom ? 5 : (props.strokeWidth ?? 2)}
                 />
-                <Rect
-                    x={0}
-                    y={0}
-                    offsetX={-(x1 + x2) / 2 + handleSize / 2}
-                    offsetY={-y1 + handleSize / 2}
-                    width={handleSize}
-                    height={handleSize}
-                    stroke={handleColor}
-                    draggable={true}
-                    onDragStart={(e) => {
-                        setIsDragging({...isDragging, bottom: true});
+                <ResizeHandle
+                    x={(x1 + x2) / 2}
+                    y={y1}
+                    handleSize={handleSize}
+                    handleColor={handleColor}
+                    resizeCursor={"ns-resize"}
+                    isDragging={isDragging.bottom}
+                    setIsDragging={(dragging) => setIsDragging({...isDragging, bottom: dragging})}
+                    onDragMove={(x, y) => {
+                        updateRect({y1: y1 + y});
+                        return {x: 0, y: y};
                     }}
-                    onDragMove={(e) => {
-                        const newY1 = y1 + e.target.y();
-                        bottomRef.current?.points([x1, newY1, x2, newY1]);
-                        rightRef.current?.points([x2, newY1, x2, y2]);
-                        leftRef.current?.points([x1, y2, x1, newY1]);
-                        e.target.setPosition({x: 0, y: e.target.y()});
-                    }}
-                    onDragEnd={(e) => {
-                        setIsDragging({...isDragging, bottom: false});
-                        const offsetY = e.target.y() / xyScales.y;
+                    onDragEnd={(x, y, reset) => {
+                        const offsetY = y / xyScales.y;
                         setBounds({
                             ...bounds,
                             bottom: bounds.bottom + offsetY,
                         });
-                        e.target.setPosition({x: 0, y: 0});
+                        updateNext.current = () => {
+                            childRef.current?.x(0);
+                            childRef.current?.y(0);
+                            childRef.current?.scaleX(1);
+                            childRef.current?.scaleY(1);
+                            reset();
+                        };
                     }}
-                    {...horzLineProps}
                 />
                 <Line
                     ref={rightRef}
@@ -195,35 +179,32 @@ export function ResizeableRect(
                     stroke={isDragging.right ? dragColor : props.stroke}
                     strokeWidth={isDragging.right ? 5 : (props.strokeWidth ?? 2)}
                 />
-                <Rect
-                    x={0}
-                    y={0}
-                    offsetX={-x2 + handleSize / 2}
-                    offsetY={-(y1 + y2) / 2 + handleSize / 2}
-                    width={handleSize}
-                    height={handleSize}
-                    stroke={handleColor}
-                    draggable={true}
-                    onDragStart={(e) => {
-                        setIsDragging({...isDragging, right: true});
+                <ResizeHandle
+                    x={x2}
+                    y={(y1 + y2) / 2}
+                    handleSize={handleSize}
+                    handleColor={handleColor}
+                    resizeCursor={"ew-resize"}
+                    isDragging={isDragging.right}
+                    setIsDragging={(dragging) => setIsDragging({...isDragging, right: dragging})}
+                    onDragMove={(x, y) => {
+                        updateRect({x2: x2 + x});
+                        return {x: x, y: 0};
                     }}
-                    onDragMove={(e) => {
-                        const newX2 = x2 + e.target.x();
-                        rightRef.current?.points([newX2, y1, newX2, y2]);
-                        bottomRef.current?.points([x1, y1, newX2, y1]);
-                        topRef.current?.points([newX2, y2, x1, y2]);
-                        e.target.setPosition({x: e.target.x(), y: 0});
-                    }}
-                    onDragEnd={(e) => {
-                        setIsDragging({...isDragging, right: false});
-                        const offsetX = e.target.x() / xyScales.x;
+                    onDragEnd={(x, y, reset) => {
+                        const offsetX = x / xyScales.x;
                         setBounds({
                             ...bounds,
                             right: bounds.right + offsetX,
                         });
-                        e.target.setPosition({x: 0, y: 0});
+                        updateNext.current = () => {
+                            childRef.current?.x(0);
+                            childRef.current?.y(0);
+                            childRef.current?.scaleX(1);
+                            childRef.current?.scaleY(1);
+                            reset();
+                        };
                     }}
-                    {...vertLineProps}
                 />
                 <Line
                     ref={topRef}
@@ -232,35 +213,32 @@ export function ResizeableRect(
                     stroke={isDragging.top ? dragColor : props.stroke}
                     strokeWidth={isDragging.top ? 5 : (props.strokeWidth ?? 2)}
                 />
-                <Rect
-                    x={0}
-                    y={0}
-                    offsetX={-(x1 + x2) / 2 + handleSize / 2}
-                    offsetY={-y2 + handleSize / 2}
-                    width={handleSize}
-                    height={handleSize}
-                    stroke={handleColor}
-                    draggable={true}
-                    onDragStart={(e) => {
-                        setIsDragging({...isDragging, top: true});
+                <ResizeHandle
+                    x={(x1 + x2) / 2}
+                    y={y2}
+                    handleSize={handleSize}
+                    handleColor={handleColor}
+                    resizeCursor={"ns-resize"}
+                    isDragging={isDragging.top}
+                    setIsDragging={(dragging) => setIsDragging({...isDragging, top: dragging})}
+                    onDragMove={(x, y) => {
+                        updateRect({y2: y2 + y});
+                        return {x: 0, y: y};
                     }}
-                    onDragMove={(e) => {
-                        const newY2 = y2 + e.target.y();
-                        topRef.current?.points([x2, newY2, x1, newY2]);
-                        rightRef.current?.points([x2, y1, x2, newY2]);
-                        leftRef.current?.points([x1, newY2, x1, y1]);
-                        e.target.setPosition({x: 0, y: e.target.y()});
-                    }}
-                    onDragEnd={(e) => {
-                        setIsDragging({...isDragging, top: false});
-                        const offsetY = e.target.y() / xyScales.y;
+                    onDragEnd={(x, y, reset) => {
+                        const offsetY = y / xyScales.y;
                         setBounds({
                             ...bounds,
                             top: bounds.top + offsetY,
                         });
-                        e.target.setPosition({x: 0, y: 0});
+                        updateNext.current = () => {
+                            childRef.current?.x(0);
+                            childRef.current?.y(0);
+                            childRef.current?.scaleX(1);
+                            childRef.current?.scaleY(1);
+                            reset();
+                        };
                     }}
-                    {...horzLineProps}
                 />
                 <Line
                     ref={leftRef}
@@ -269,37 +247,91 @@ export function ResizeableRect(
                     stroke={isDragging.left ? dragColor : props.stroke}
                     strokeWidth={isDragging.left ? 5 : (props.strokeWidth ?? 2)}
                 />
-                <Rect
-                    x={0}
-                    y={0}
-                    offsetX={-x1 + handleSize / 2}
-                    offsetY={-(y1 + y2) / 2 + handleSize / 2}
-                    width={handleSize}
-                    height={handleSize}
-                    stroke={handleColor}
-                    draggable={true}
-                    onDragStart={(e) => {
-                        setIsDragging({...isDragging, left: true});
+                <ResizeHandle
+                    x={x1}
+                    y={(y1 + y2) / 2}
+                    handleSize={handleSize}
+                    handleColor={handleColor}
+                    resizeCursor={"ew-resize"}
+                    isDragging={isDragging.left}
+                    setIsDragging={(dragging) => setIsDragging({...isDragging, left: dragging})}
+                    onDragMove={(x, y) => {
+                        updateRect({x1: x1 + x});
+                        return {x: x, y: 0};
                     }}
-                    onDragMove={(e) => {
-                        const newX1 = x1 + e.target.x();
-                        leftRef.current?.points([newX1, y2, newX1, y1]);
-                        bottomRef.current?.points([newX1, y1, x2, y1]);
-                        topRef.current?.points([x2, y2, newX1, y2]);
-                        e.target.setPosition({x: e.target.x(), y: 0});
-                    }}
-                    onDragEnd={(e) => {
-                        setIsDragging({...isDragging, left: false});
-                        const offsetX = e.target.x() / xyScales.x;
+                    onDragEnd={(x, y, reset) => {
+                        const offsetX = x / xyScales.x;
                         setBounds({
                             ...bounds,
                             left: bounds.left + offsetX,
                         });
-                        e.target.setPosition({x: 0, y: 0});
+                        updateNext.current = () => {
+                            childRef.current?.x(0);
+                            childRef.current?.y(0);
+                            childRef.current?.scaleX(1);
+                            childRef.current?.scaleY(1);
+                            reset();
+                        };
                     }}
-                    {...vertLineProps}
                 />
             </Group>
         </Group>
+    );
+}
+
+function ResizeHandle(
+    {
+        x, y,
+        handleSize,
+        handleColor,
+        resizeCursor,
+        isDragging, setIsDragging,
+        onDragMove, onDragEnd,
+    }: Readonly<{
+        x: number,
+        y: number,
+        handleSize: number,
+        handleColor: string,
+        resizeCursor: string,
+        isDragging: boolean,
+        setIsDragging: (isDragging: boolean) => void,
+        onDragMove: (x: number, y: number) => {x: number, y: number},
+        onDragEnd: (x: number, y: number, reset: () => void) => void,
+    }>
+) {
+
+    return (
+        <Rect
+            x={0}
+            y={0}
+            offsetX={-x + handleSize / 2}
+            offsetY={-y + handleSize / 2}
+            width={handleSize}
+            height={handleSize}
+            stroke={handleColor}
+            draggable={true}
+            onDragStart={(e) => {
+                setIsDragging(true);
+            }}
+            onDragMove={(e) => {
+                e.target.setPosition(onDragMove(e.target.x(), e.target.y()));
+            }}
+            onDragEnd={(e) => {
+                setIsDragging(false);
+                onDragEnd(e.target.x(), e.target.y(), () => {e.target.setPosition({x: 0, y: 0});});
+            }}
+            onMouseEnter={(e: Konva.KonvaEventObject<MouseEvent>) => {
+                const container = e.target.getStage()?.container();
+                if (container) {
+                    container.style.cursor = resizeCursor;
+                }
+            }}
+            onMouseLeave={(e: Konva.KonvaEventObject<MouseEvent>) => {
+                const container = e.target.getStage()?.container();
+                if (container) {
+                    container.style.cursor = "default";
+                }
+            }}
+        />
     );
 }
